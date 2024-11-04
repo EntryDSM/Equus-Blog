4067522b1558fa3761a456c119ed66a89aeb1f3a
박주원 <qkrwndnjs7308@gmail.com>
2024-11-04 13:57:57 +0900

    올바른 status 코드 반환

--- blog/src/main/kotlin/com/example/blog/domain/post/presentation/controller/PostController.kt
+++ blog/src/main/kotlin/com/example/blog/domain/post/presentation/controller/PostController.kt
@@ -2,14 +2,18 @@ package com.example.blog.domain.post.presentation.controller
 
 import com.example.blog.domain.post.domain.Post
 import com.example.blog.domain.post.presentation.dto.request.PostCreateRequest
+import com.example.blog.domain.post.presentation.dto.request.PostUpdateRequest
 import com.example.blog.domain.post.service.PostService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -18,6 +22,7 @@ import java.util.UUID
 class PostController(private val postService: PostService) {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createPost(
         @RequestBody @Valid postRequest: PostCreateRequest,
     ): Post {
@@ -25,6 +30,7 @@ class PostController(private val postService: PostService) {
     }
 
     @GetMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
     fun getPostById(
         @PathVariable postId: UUID,
     ): Post {
@@ -32,10 +38,20 @@ class PostController(private val postService: PostService) {
     }
 
     @PatchMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
     fun updatePost(
         @PathVariable postId: UUID,
         @RequestBody @Valid postRequest: PostUpdateRequest,
     ): Post {
         return postService.updatePost(postId, postRequest)
     }
+
+    @DeleteMapping("/{postId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deletePost(
+        @PathVariable postId: UUID,
+    ) {
+        postService.deletePost(postId)
+    }
+}
 }
